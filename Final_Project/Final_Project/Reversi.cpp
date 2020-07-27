04b1136011107d3761c59bac653cfcd6e345a02b
knishibe <32073936+knishibe@users.noreply.github.com>
2020-07-27 11:47:57 -0700

    setup done

--- Final_Project/Final_Project/Reversi.cpp
+++ Final_Project/Final_Project/Reversi.cpp
@@ -8,3 +8,39 @@ Reversi::Reversi() {
 bool Reversi::checkWin() {
 
 }
+
+void Reversi::flip() {
+
+}
+
+void Reversi::display_board() {
+
+}
+
+void Reversi::display_moves() {
+
+}
+
+void Reversi::computer_turn(bool withHeuristic) {
+
+}
+
+void Reversi::human_turn() {
+
+}
+
+void Reversi::change_turn() {
+
+}
+
+int Reversi::heuristic() {
+
+}
+
+tuple<int, int, int> Reversi::randomPlayouts(int move) {
+
+}
+
+vector<int> Reversi::possible_moves() {
+
+}
--- Final_Project/Final_Project/Reversi.h
+++ Final_Project/Final_Project/Reversi.h
@@ -1,10 +1,26 @@
+#include <stdio.h>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
 class Reversi {
 
 public:
 	Reversi();
 	bool checkWin();
+	void flip();
+	void display_board();
+	void display_moves();
+	void computer_turn(bool withHeuristic); 
+	void human_turn();
+	void change_turn();
 
 protected:
 	int board[8][8];
+	int turn;
+	int heuristic();
+	tuple<int, int, int> randomPlayouts(int move); //Emily
+	vector<int> possible_moves();
 };
 
